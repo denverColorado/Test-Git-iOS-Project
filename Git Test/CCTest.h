f423ccd1c7ba234f9d3f04931aa9475cb6b0225d
Che' Bustos <chebustos@Ches-MacBook.local>
2014-07-05 13:31:20 -0600

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Che' Bustos on 7/5/14.
+//  Copyright (c) 2014 Colorado Coders. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Che' Bustos on 7/5/14.
+//  Copyright (c) 2014 Colorado Coders. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
